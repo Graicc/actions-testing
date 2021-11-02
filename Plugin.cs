7734ee170c267d763a4bf7bc7cb2761e6dd44ec4
Graicc <33105645+Graicc@users.noreply.github.com>
2021-11-01 21:57:47 -0400

    Add template project

---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace actions_testing
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+		{
+            HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+        void Update()
+		{
+
+		}
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace actions_testing
+{
+	class PluginInfo
+	{
+		public const string GUID = "com.graic.gorillatag.actions-testing";
+		public const string Name = "actions_testing";
+		public const string Version = "1.0.0";
+	}
+}
